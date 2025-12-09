ac6646e24f731919b3d58a442851dd75c78ab6bb
Lunyaaa~ <prasol258@gmail.com>
2025-12-09 21:11:30 +0000

    build just the kernel

--- flake.nix
+++ flake.nix
@@ -14,11 +14,8 @@
       ];
     };
   in {
-    checks.${system} = pkgs.lib.pipe pkgs.cachyosKernels.linuxPackages-cachyos-latest [
-      (pkgs.lib.mapAttrs (_: v: builtins.tryEval v))
-      (pkgs.lib.filterAttrs (_: res: res.success))
-      (pkgs.lib.mapAttrs (_: res: res.value))
-      (pkgs.lib.filterAttrs (_: v: pkgs.lib.isDerivation (v.kernel or null)))
-    ];
+    checks.${system} = {
+      inherit (pkgs.cachyosKernels.linuxPackages-cachyos-latest) kernel;
+    };
   };
 }
