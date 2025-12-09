88e6eff9a983d37b2f422e6fc2d231a238184d6b
Lunyaaa~ <prasol258@gmail.com>
2025-12-09 21:20:36 +0000

    revert

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable-small";
-    nix-cachyos-kernel = {
-      url = "github:xddxdd/nix-cachyos-kernel";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel";
   };
 
-  outputs = { nix-cachyos-kernel, ... }: 
-    checks."x86_64-linux" = {
+  outputs = { nixpkgs, nix-cachyos-kernel, ... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        nix-cachyos-kernel.overlays.default
+      ];
+    };
+  in {
+    checks.${system} = {
       inherit (pkgs.cachyosKernels.linuxPackages-cachyos-latest) kernel;
     };
   };
